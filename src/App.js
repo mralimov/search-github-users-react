f3d1563451337038e21d44740bc6b8fd06383a87
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-05-11 22:25:08 -0400

    fetching API APP.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Form from './components/form/Form';
+import './App.scss';
+import Loader from './components/loader/Loader';
+const BASE_URL = 'https://api.github.com/search/users';
 
 const App = () => {
-  return <h1>App</h1>;
+  const [userData, setUserData] = useState([]);
+  const [isLoding, setIsLoading] = useState(true);
+  const [userName, setUserName] = useState('');
+  const [radioInput, setRadioInput] = useState('');
+
+  useEffect(() => {
+    if (!userName) return;
+    (async () => {
+      try {
+        if (radioInput === 'org') {
+          const response = await fetch(`${BASE_URL}?q=${userName}+type:org`);
+          const data = await response.json();
+          setUserData(data.items);
+        } else {
+          const response = await fetch(`${BASE_URL}?q=${userName}`);
+          const data = await response.json();
+          setUserData(data.items);
+        }
+
+        console.log(userName);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [radioInput, userName]);
+
+  console.log(userData);
+  return <Form setUserName={setUserName} setRadioInput={setRadioInput} />;
 };
 
 export default App;
