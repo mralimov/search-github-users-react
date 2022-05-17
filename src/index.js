6e8775ad5ef8386538f3aa72b0b48462a2c00dfc
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-05-17 12:20:51 -0400

    Wrapping App with UserProvider on React render

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import reactDOM from 'react-dom/client';
 import App from './App';
 import './App.scss';
+import UserContext from './components/state-context/UserContext';
+import UserProvider from './components/state-context/UserProvider';
 
 const root = reactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <UserProvider>
+    <App />
+  </UserProvider>
+);
