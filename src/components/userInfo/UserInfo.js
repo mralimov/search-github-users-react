eef09034f1a4055d7c88956cb319c22e6a72063c
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-05-18 01:03:49 -0400

    disabling UserInfo component

--- src/components/userInfo/UserInfo.js
+++ src/components/userInfo/UserInfo.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import UserContext from '../state-context/UserContext';
 import './_userInfo.scss';
+import UserCard from '../UI/UserCard';
 
 const UserInfo = () => {
   const { userData } = useContext(UserContext);
@@ -9,13 +10,14 @@ const UserInfo = () => {
       {userData.map((user) => {
         const { avatar_url, id, login } = user;
         return (
-          <div className='user-card' key={id}>
-            <img className='user-image' alt='user' src={avatar_url} />
+          <UserCard userData={userData} />
+          // <div className='user-card' key={id}>
+          //   <img className='user-image' alt='user' src={avatar_url} />
 
-            <div className='user-info'>
-              <h2 className='username'>{login}</h2>
-            </div>
-          </div>
+          //   <div className='user-info'>
+          //     <h2 className='username'>{login}</h2>
+          //   </div>
+          // </div>
         );
       })}
     </>
