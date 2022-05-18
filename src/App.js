a253bf741e4e8911863f666562852de38c8052db
Donnie Alimov <47795746+mralimov@users.noreply.github.com>
2022-05-18 01:01:41 -0400

    implementing Loading logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Form from './components/form/Form';
 import './App.scss';
 import Loader from './components/loader/Loader';
 import UserInfo from './components/userInfo/UserInfo';
 import useFetch from './components/useFetch/useFetch';
+import UserCard from './components/UI/UserCard';
+import UserContext from './components/state-context/UserContext';
 
 const App = () => {
   const { loading } = useFetch();
+  const { isLoading } = useContext(UserContext);
+
+  console.log(isLoading);
   return (
     <>
       <Form />
-      {loading && <Loader />}
-      <UserInfo />
+      {isLoading && <Loader />}
+      <div className='result'>
+        <UserCard />
+      </div>
+      {/* <UserInfo /> */}
     </>
   );
 };
